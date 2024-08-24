63acaf2d7c6d1835ee795e72aa139613ee7582d7
UFifty50sh <UFifty50.github@gmail.com>
2024-08-24 12:12:28 +0100

    Create spdlog_config.h

---
+++ include/spdlog/spdlog_config.h
@@ -0,0 +1,5 @@
+//-----------------------------------------------------------------------------------------
+//
+// This is a placement file to prevent compile errors, give your own config options
+// (i.e. I dont want to lean enough lua to generate this file through premake)
+//-----------------------------------------------------------------------------------------
